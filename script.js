4324c5d3ba60a9b0fa1df0c8f6f5e7cdc4a8390d
Diana <monteiro.diana18@gmail.com>
2017-10-05 22:22:22 -0300

    Header on mobile

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
 function scrollTo(sectionId) {
   document.getElementById(sectionId).scrollIntoView(true);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var upArrow = document.getElementById("up-arrow");
+  var downArrow = document.getElementById("down-arrow");
+  var sectionAnchors = document.getElementsByClassName('header-section-anchor');
+  var sectionIds = Array.prototype.map.call(document.getElementsByClassName('section'), function (element) {
+    return element.id;
+  });
+
+  var sectionIndex;
+
+  setSectionIndex(0);
+
+  upArrow.addEventListener("click", function () {
+    setSectionIndex(Math.max(0, sectionIndex - 1));
+    scrollTo(sectionIds[sectionIndex]);
+  });
+
+  downArrow.addEventListener("click", function () {
+    setSectionIndex(Math.min(sectionIds.length - 1, sectionIndex + 1));
+    scrollTo(sectionIds[sectionIndex]);
+  });
+
+  function showCurrentSectionInHeader() {
+    for (var i = 0; i < sectionAnchors.length; i++) {
+      if (i === sectionIndex) {
+        sectionAnchors[i].style.display = '';
+      } else {
+        sectionAnchors[i].style.display = 'none';
+      }
+    }
+  }
+
+  function setSectionIndex(newSectionIndex) {
+    sectionIndex = newSectionIndex;
+    if (sectionIndex === 0) {
+      hideArrow(upArrow);
+    } else if (sectionIndex === sectionIds.length - 1) {
+      hideArrow(downArrow);
+    } else {
+      showArrow(upArrow);
+      showArrow(downArrow);
+    }
+    showCurrentSectionInHeader();
+  }
+
+  function hideArrow(arrow) {
+    arrow.style.display = 'none';
+  }
+
+  function showArrow(arrow) {
+    arrow.style.display = '';
+  }
+})
